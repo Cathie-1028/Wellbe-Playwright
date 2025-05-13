e45ce21f8ddaea2f1ca29f5cc7b1bf561b0a072a
Cathie-1028 <catherine@mayan.com.ph>
2025-05-13 15:18:36 +0800

    Update manual-invite.spec.ts

--- tests/manual-invite.spec.ts
+++ tests/manual-invite.spec.ts
@@ -1,5 +1,21 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  // Recording...
+  await page.goto('https://wellbe-admin.vercel.app/sign-in');
+  await page.getByRole('textbox', { name: 'Email' }).click();
+  await page.getByRole('textbox', { name: 'Email' }).fill('andrew@mayan.com.ph');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('123456');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('tab', { name: 'Employees' }).click();
+  await page.getByRole('button', { name: '+ Invite' }).click();
+  await page.getByLabel('Department').selectOption('Human Resources');
+  await page.getByRole('textbox', { name: 'First Name' }).click();
+  await page.getByRole('textbox', { name: 'First Name' }).fill('Catherine Anne');
+  await page.getByRole('textbox', { name: 'Last Name' }).click();
+  await page.getByRole('textbox', { name: 'Last Name' }).fill('Alcala');
+  await page.getByRole('textbox', { name: 'Company Email' }).click();
+  await page.getByRole('textbox', { name: 'Company Email' }).fill('catherineanne0128@gmail.com');
+  await page.getByRole('button', { name: 'Invite', exact: true }).click();
+  await page.getByRole('button', { name: 'Invite', exact: true }).click();
 });

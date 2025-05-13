66977b6adc68b88dcf28b3b8e0ce3e847fc41e61
Cathie-1028 <catherine@mayan.com.ph>
2025-05-13 17:11:10 +0800

    Updated

--- tests/pop-up-notiifcation-batch.spec.ts
+++ tests/pop-up-notiifcation-batch.spec.ts
@@ -1,11 +1,11 @@
 import { test, expect } from '@playwright/test';
 
 test('pop-up', async ({ page }) => {
-  await page.goto('https://wellbe-admin.vercel.app/sign-in');
+  await page.goto('http://localhost:5173/sign-in');
   await page.getByRole('textbox', { name: 'Email' }).click();
-  await page.getByRole('textbox', { name: 'Email' }).fill('andrew@mayan.com.ph');
+  await page.getByRole('textbox', { name: 'Email' }).fill('catherine@mayan.com.ph');
   await page.getByRole('textbox', { name: 'Email' }).press('Tab');
-  await page.getByRole('textbox', { name: 'Password' }).fill('123456');
+  await page.getByRole('textbox', { name: 'Password' }).fill('Mayan@123!!');
   await page.getByRole('button', { name: 'Login' }).click();
   await page.getByRole('button', { name: 'Send Batch Survey' }).click();
   await page.getByText('Are you sure you want to').click();

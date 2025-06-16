ae9c856067f3ebb68018e3f76268b0628e89897a
Cathie-1028 <catherine@mayan.com.ph>
2025-06-16 21:42:36 +0800

    updated next quick check information display

--- tests/next-quick-info-display.spec.ts
+++ tests/next-quick-info-display.spec.ts
@@ -1,5 +1,13 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  // Recording...
+  await page.goto('https://employee-wellbe-develop.vercel.app/sign-in');
+  await page.getByRole('textbox', { name: 'Email Address' }).click();
+  await page.getByRole('textbox', { name: 'Email Address' }).fill('catherine@mayan.com.ph');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('123456');
+  await page.getByRole('button', { name: 'Log in' }).click();
+  await page.goto('https://employee-wellbe-develop.vercel.app/');
+  await page.getByText('You have finished all your quick checks, your next Quick Check will be').click();
+  await page.getByText('You have finished all your quick checks, your next Quick Check will be').click();
 });
